25ab52e75cd54a0e81f9d1b8367d95479204076b
Jonatan Dellagostin <Jonatan_Dellagostin@banrisul.com.br>
2016-11-11 17:36:01 -0200

    Adding more PEP8 compliance to the code.

--- tests/test_loggers.py
+++ tests/test_loggers.py
@@ -1,60 +1,72 @@
 import unittest
-from loggers import Loggers
-import bz2file
 import shutil
 import re
 import os
+import bz2file
+from loggers import Loggers
 
-def decompBz2(logFile):
-    with bz2file.open(logFile) as mfile:
-        datalog=mfile.read()
+def decomp_bz2(log_file):
+    with bz2file.open(log_file) as mfile:
+        datalog = mfile.read()
     return datalog
 
 class LoggersTest(unittest.TestCase):
-    @classmethod  
+    @classmethod
     def setUpClass(cls):
-        cls.logpath='/tmp/logTest'
+        cls.logpath = '/tmp/logTest'
         os.mkdir(cls.logpath)
-        cls.logTest = Loggers('logTest', logFolderPath = cls.logpath)
+        cls.logTest = Loggers('logTest', logFolderPath=cls.logpath)
         cls.logTest.setLogRotateHandler(True)
 
-    @classmethod  
+    @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.logpath)
 
-    def performRegexTest(self, logLevel, logFunction, logMessage, logLevelDst):
-        self.logTest.setLogLevel(logLevel)
-        logFunction(logMessage)
-        datalog = decompBz2(self.logpath+'/logTest.'+logLevelDst+'.log.bz2')
-        match=re.match('Log: '+logMessage+' | Log level:'+logLevel+' | Date:\d{2}\/\d{2}\/\d{4} d{2}:\d{2}:\d{2}',datalog.splitlines()[-1])
+    def perform_regex_test(self, log_level, log_function, log_message, log_level_dst):
+        self.logTest.setLogLevel(log_level)
+        log_function(log_message)
+        datalog = decomp_bz2(self.logpath+'/logTest.'+log_level_dst+'.log.bz2')
+        match = re.match('Log: '+log_message+' | Log level:'+log_level+
+                         r' | Date:\d{2}\/\d{2}\/\d{4} d{2}:\d{2}:\d{2}',
+                         datalog.splitlines()[-1])
         return match
 
-    def testDebugLog(self):
-        self.assertIsNotNone(self.performRegexTest('DEBUG', self.logTest.log.debug, 'Debug Test.', 'debug'))
+    def test_debug_log(self):
+        self.assertIsNotNone(self.perform_regex_test('DEBUG', self.logTest.log.debug,
+                                                     'Debug Test.', 'debug'))
 
-    def testInfoLog(self):
-        self.assertIsNotNone(self.performRegexTest('INFO', self.logTest.log.info, 'Info Test.', 'debug'))
+    def test_info_log(self):
+        self.assertIsNotNone(self.perform_regex_test('INFO', self.logTest.log.info,
+                                                     'Info Test.', 'debug'))
 
-    def testWarningLog(self):
-        self.assertIsNotNone(self.performRegexTest('WARNING', self.logTest.log.warning, 'Warning Test.', 'debug'))
+    def test_warning_log(self):
+        self.assertIsNotNone(self.perform_regex_test('WARNING', self.logTest.log.warning,
+                                                     'Warning Test.', 'debug'))
 
-    def testErrorLog(self):
-        self.assertIsNotNone(self.performRegexTest('ERROR', self.logTest.log.error, 'Error Test.', 'error'))
+    def test_error_log(self):
+        self.assertIsNotNone(self.perform_regex_test('ERROR', self.logTest.log.error,
+                                                     'Error Test.', 'error'))
 
-    def testCriticalLog(self):
-        self.assertIsNotNone(self.performRegexTest('CRITICAL', self.logTest.log.critical, 'Critical Test.', 'error'))
+    def test_critical_log(self):
+        self.assertIsNotNone(self.perform_regex_test('CRITICAL', self.logTest.log.critical,
+                                                     'Critical Test.', 'error'))
 
-    def testLogHierarchy(self):
-        logMessage ='Debug log in CRITICAL log level Test.'
-        self.assertIsNone(self.performRegexTest('CRITICAL', self.logTest.log.debug,logMessage, 'error'))
-        logMessage ='Info log in CRITICAL log level Test.'
-        self.assertIsNone(self.performRegexTest('CRITICAL', self.logTest.log.info,logMessage, 'error'))
-        logMessage ='Warning log in CRITICAL log level Test.'
-        self.assertIsNone(self.performRegexTest('CRITICAL', self.logTest.log.warning,logMessage, 'error'))
-        logMessage ='Error log in CRITICAL log level Test.'
-        self.assertIsNone(self.performRegexTest('CRITICAL', self.logTest.log.error,logMessage, 'error'))
-        logMessage ='Critical log in CRITICAL log level Test.'
-        self.assertIsNotNone(self.performRegexTest('CRITICAL', self.logTest.log.critical,logMessage, 'error'))
+    def test_log_hierarchy(self):
+        log_message = 'Debug log in CRITICAL log level Test.'
+        self.assertIsNone(self.perform_regex_test('CRITICAL', self.logTest.log.debug,
+                                                  log_message, 'error'))
+        log_message = 'Info log in CRITICAL log level Test.'
+        self.assertIsNone(self.perform_regex_test('CRITICAL', self.logTest.log.info,
+                                                  log_message, 'error'))
+        log_message = 'Warning log in CRITICAL log level Test.'
+        self.assertIsNone(self.perform_regex_test('CRITICAL', self.logTest.log.warning,
+                                                  log_message, 'error'))
+        log_message = 'Error log in CRITICAL log level Test.'
+        self.assertIsNone(self.perform_regex_test('CRITICAL', self.logTest.log.error,
+                                                  log_message, 'error'))
+        log_message = 'Critical log in CRITICAL log level Test.'
+        self.assertIsNotNone(self.perform_regex_test('CRITICAL', self.logTest.log.critical,
+                                                     log_message, 'error'))
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     unittest.main()
